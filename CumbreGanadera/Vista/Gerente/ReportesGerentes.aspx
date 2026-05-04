69b1c4b6e7e5b91051cba5926c0ea39b7278b5d9
THE-LEVITE <halfonsokyar03@gmail.com>
2026-05-04 09:29:22 -0500

    v0.4_Creacion y adelanto del envio de las soliciudes_TheJotaro777_040526

---
+++ CumbreGanadera/Vista/Gerente/ReportesGerentes.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Vista/Estructura.Master" AutoEventWireup="true" CodeBehind="ReportesGerentes.aspx.cs" Inherits="CumbreGanadera.Vista.Gerente.ReportesGerentes" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" href="../Source/CSS/StyleGerente.css">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="container">
+        <h3>REPORTES OPERATIVOS</h3>
+        <p class="subtitle">Gestione y visualice el historial operativo de la hacienda en tiempo real.</p>
+
+        <div class="card">
+
+            <h4><i class="bi bi-file-earmark-bar-graph" style="color: green"></i>CREAR NUEVO REPORTE OPERATIVO</h4>
+
+            <div class="form-row">
+
+
+
+
+                <div class="form-group col-12">
+                    <label>TITULO SOLICITUD</label>
+                    <asp:TextBox ID="txtTitulo" runat="server" CssClass="textarea" TextMode="MultiLine"></asp:TextBox>
+                </div>
+
+
+                <div class="form-group">
+                    <label>DESCRIPCIÓN DE LA ACTIVIDAD / INCIDENCIAS</label>
+                    <asp:TextBox ID="txtDescripcion" runat="server" CssClass="textarea" TextMode="MultiLine"></asp:TextBox>
+                </div>
+
+                <div class="form-group">
+                    <label>FECHA SOLICITUD</label>
+                    <asp:TextBox ID="txtFecha" runat="server" CssClass="input" TextMode="Date"></asp:TextBox>
+                </div>
+
+            </div>
+
+            <div class="btn-container">
+                <asp:Button ID="btnRegistrar" runat="server" Text="📤 ENVIAR SOLICITUD" CssClass="btnEnviar" />
+            </div>
+
+        </div>
+    </div>
+</asp:Content>
---
+++ CumbreGanadera/Vista/Gerente/ReportesGerentes.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace CumbreGanadera.Vista.Gerente
+{
+
+
+    public partial class ReportesGerentes
+    {
+
+        /// <summary>
+        /// Control txtTitulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTitulo;
+
+        /// <summary>
+        /// Control txtDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDescripcion;
+
+        /// <summary>
+        /// Control txtFecha.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFecha;
+
+        /// <summary>
+        /// Control btnRegistrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRegistrar;
+    }
+}
